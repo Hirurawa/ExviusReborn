8c1a155187a53d89982dd5a12fcd602399fc6b35
Siteri Dávid <siteridavid@gmail.com>
2026-03-30 16:58:19 +0200

    Merge pull request #1 from Hirurawa/feature/login-registration-ui-13464028426292010277
    Add Login and Registration UI

--- godot/server_connection.gd
+++ godot/server_connection.gd
@@ -9,7 +9,29 @@ var _client := Nakama.create_client(KEY, "127.0.0.1", 7350, "http")
 func authenticate_async(email: String, password: String) -> int:
 	var result := OK
 	
-	var new_session: NakamaSession = await(_client.authenticate_email_async(email, password, email, false))
+	var new_session: NakamaSession = await(_client.authenticate_email_async(email, password, "", false))
+
+	if not new_session.is_exception():
+		_session = new_session
+	else:
+		result = new_session.get_exception().status_code
+
+	return result
+
+func get_account_async():
+	if _session == null or _session.is_expired():
+		return null
+
+	var account = await(_client.get_account_async(_session))
+	if account.is_exception():
+		return null
+
+	return account as NakamaAPI.ApiAccount
+
+func register_async(email: String, password: String, username: String) -> int:
+	var result := OK
+
+	var new_session: NakamaSession = await(_client.authenticate_email_async(email, password, username, true))
 	
 	if not new_session.is_exception():
 		_session = new_session

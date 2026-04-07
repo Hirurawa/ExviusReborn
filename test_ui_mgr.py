d11f260a298917c3ef7c7d52b709a34f61bbac76
Siteri David <siteridavid@gmail.com>
2026-04-07 15:32:02 +0200

    Removed leftover test py file

--- test_ui_mgr.py
+++
@@ -1,4 +0,0 @@
-import sys
-# Just thinking: set_root calls queue_free on everything in the stack, clears the array, and then calls push().
-# Wait, if set_root calls queue_free on everything in the stack, but the array is cleared, `if not _menu_stack.is_empty():` is false in `push()`.
-# So it pushes the new menu successfully.

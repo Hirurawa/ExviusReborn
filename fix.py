57befccc6315297dd64c2855ebe34bc2ed49ad62
Siteri David <siteridavid@gmail.com>
2026-04-12 20:58:21 +0200

    Removed unnecessary files

--- fix.py
+++
@@ -1,9 +0,0 @@
-with open('./godot/features/battle/ui/combat_unit_panel.gd', 'r') as f:
-    lines = f.readlines()
-
-new_lines = []
-for line in lines:
-    new_lines.append(line)
-
-with open('./godot/features/battle/ui/combat_unit_panel.gd', 'w') as f:
-    f.writelines(new_lines)

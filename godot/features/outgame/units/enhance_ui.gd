a261e9f759275ec07bf80e2b064b63a3011f8851
Siteri David <siteridavid@gmail.com>
2026-04-27 18:18:38 +0200

    Enhance units polish

--- godot/features/outgame/units/enhance_ui.gd
+++ godot/features/outgame/units/enhance_ui.gd
@@ -3,9 +3,21 @@ extends Control
 @onready var base_unit_id_label: Label = $EnhanceFlowRoot/BaseUnitIdLabel
 @onready var base_unit_sprite: TextureRect = $EnhanceFlowRoot/BaseUnitDisplay/BaseUnitSprite
 @onready var materials_container: HBoxContainer = $EnhanceFlowRoot/MaterialPedestalsContainer
-@onready var cancel_button: Button = $EnhanceFlowRoot/EnhanceActionBar/CancelButton
-@onready var clear_button: Button = $EnhanceFlowRoot/EnhanceActionBar/ClearButton
-@onready var confirm_button: Button = $EnhanceFlowRoot/EnhanceActionBar/ConfirmButton
+@onready var cancel_button: Button = $EnhanceFlowRoot/UnitNamebgChara/UnitMinibutton1
+@onready var clear_button: Button = $unit_mix_ui_bg/unit_mix_button_clear
+@onready var confirm_button: Button = $unit_mix_ui_bg/unit_mix_button_union
+
+@onready var UnitLevel: Label = $UnitLevel/UnitLevel
+
+@onready var HP: Label = $unit_statusbg/unit_status_label_hp/unit_status_ext_hp_now_number
+@onready var MP: Label = $unit_statusbg/unit_status_label_mp/unit_status_ext_mp_now_number
+@onready var ATK: Label = $unit_statusbg/unit_status_label_attack/unit_status_ext_attack_now_number
+@onready var DEF:Label = $unit_statusbg/unit_status_label_defense/unit_status_ext_defense_now_number
+@onready var MAG: Label = $unit_statusbg/unit_status_label_magic/unit_status_ext_magic_now_number
+@onready var SPR: Label = $unit_statusbg/unit_status_label_mnd/unit_status_ext_mnd_now_number
+
+@onready var LBName: Label = $UnitLbframe/LBName
+@onready var TMName: Label = $TM/unit_mix_bonds_name
 
 var base_unit_instance_id: String = ""
 var base_unit_inst: Dictionary = {}
@@ -52,6 +64,7 @@ func _connect_buttons() -> void:
 func _refresh_base_unit_ui() -> void:
 	base_unit_id_label.text = "Base Instance ID: %s" % base_unit_instance_id
 	base_unit_sprite.texture = _get_unit_texture(base_unit_inst)
+	_display_unit_stats(base_unit_inst)
 
 func _redraw_material_slots() -> void:
 	for slot in _pedestal_slots:
@@ -107,6 +120,7 @@ func _on_clear_pressed() -> void:
 	_redraw_material_slots()
 
 func _on_confirm_pressed() -> void:
+	print("CONFIRM")
 	# UI state only for now. Backend enhancement processing will be wired later.
 	pass
 
@@ -131,3 +145,45 @@ func _get_unit_texture(unit_inst: Dictionary) -> Texture2D:
 	var tex: Texture2D = ResourceLoader.load(img_path) as Texture2D
 	_texture_cache[img_path] = tex
 	return tex
+
+func _display_unit_stats(unit_inst: Dictionary) -> void:
+	if unit_inst.is_empty():
+		return
+	
+	UnitLevel.text = str(int(unit_inst.level))
+	var currentRarity = int(unit_inst.current_rarity)
+	var maxLevel = StatCalculator.RARITY_MAX_LEVELS.get(int(currentRarity), 15)
+	UnitLevel.text += " / " + str(maxLevel)
+	var xp = int(unit_inst.xp)
+	var currentXp = int(unit_inst.xp)
+	var xpForNextLevel = int(unit_inst.next_xp)
+	var lb_id = str(int(unit_inst.get("limitburst_id", "")))
+	if lb_id != "" and DataManager.game_data_limitbursts.has(lb_id):
+		LBName.text = DataManager.game_data_limitbursts[lb_id].get("name", "Unknown Limit Burst")
+	
+	var tmr_data = unit_inst.get("TMR")
+	if tmr_data != null and typeof(tmr_data) == TYPE_ARRAY and tmr_data.size() >= 2:
+		var tmr_type = tmr_data[0]
+		var tmr_id = str(int(tmr_data[1]))
+		var tmr_name = "Unknown Reward"
+
+		if tmr_type == "EQUIP":
+			if DataManager.game_data_equipment.has(tmr_id):
+				var eq_data = DataManager.game_data_equipment[tmr_id]
+				tmr_name = eq_data.get("name", tmr_name)
+		elif tmr_type == "MATERIA":
+			if DataManager.game_data_materia.has(tmr_id):
+				var mat_data = DataManager.game_data_materia[tmr_id]
+				tmr_name = mat_data.get("name", tmr_name)
+
+		TMName.text = tmr_name
+	else:
+		TMName.text = "None"
+	
+	
+	HP.text = str(unit_inst.final_stats["stats"].get("HP"))
+	MP.text = str(unit_inst.final_stats["stats"].get("MP"))
+	ATK.text = str(unit_inst.final_stats["stats"].get("ATK"))
+	DEF.text = str(unit_inst.final_stats["stats"].get("DEF"))
+	MAG.text = str(unit_inst.final_stats["stats"].get("MAG"))
+	SPR.text = str(unit_inst.final_stats["stats"].get("SPR"))

ec439336dea404fd2a0f5886f7dd9c41f39c3a36
Siteri David <siteridavid@gmail.com>
2026-06-30 09:21:26 +0200

    removed unused file

--- quest_list_update.py
+++
@@ -1,7 +0,0 @@
-import re
-
-with open("godot/features/quest/quest_list_dialog.gd", "r") as f:
-    content = f.read()
-
-# We need to add town_id parameter and handle signals for map_ui.gd refresh
-# Wait, actually we can just emit a signal from QuestListDialog or do things inline
